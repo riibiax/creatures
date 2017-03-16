14101e9a83b4287eb005b6c3e9b184d4889131b5
riibiax <info@earove.info>
2017-03-16 16:38:57 +0100

    test

---
+++ config.js
@@ -0,0 +1,17 @@
+var config = {
+	twitter: {
+		  consumer_key:         't2EwDeOMJrKlggIJVDQKVHDuK',
+  		consumer_secret:      'meWJI7UWBb4nQCWKqq9BCiKQ4RmiZIUiL5sCQFQYcaz2Xd4mmL',
+  		access_token:         '826079850951741440-yvRudCJ2YvVdxm2Ei7bTSaWmGb29UVY',
+  		access_token_secret:  '7uieHQxG9xDucP4bFG5lOcEQO1zjVw2ga9qljm62alfVH',
+  		timeout_ms:           60*1000
+	},
+	firebase: {
+    	apiKey: 				      "AIzaSyBWusdl4wVOg9qPzRUpEwLx4BAMAcf3bQ4",
+    	authDomain: 			    "swissmountains-a030c.firebaseapp.com",
+    	databaseURL: 			    "https://swissmountains-a030c.firebaseio.com",
+    	storageBucket: 			  "swissmountains-a030c.appspot.com",
+    	messagingSenderId: 		"1008488826399"
+	}
+};
+module.exports = config;
