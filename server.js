5b4fbc636d3462a46fe447b2d0d90bd10e2f30e8
riibiax <info@earove.info>
2017-03-16 16:02:13 +0100

    test

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
 var express = require('express');
 var socket = require('socket.io');
-var firebase = require('firebase');
-var exec = require('child_process').exec;
-var fs = require('fs');
-var config= require('./config');
-var utils = require ('./utilsNode');
+
